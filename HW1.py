f74a77850c56406384799c5364d3922b41a8a6fd
Jaime Gil <jgil3@cougarnet.uh.edu>
2020-09-16 11:44:09 -0500

    Homework 1

---
+++ HW1.py
@@ -0,0 +1,33 @@
+'''
+Jaime Gil
+1788946
+'''
+
+print("Birthday Calculator")
+print("Current day")
+curr_month = int(input("Month: "))
+curr_day = int(input("Day: "))
+curr_year = int(input("Year: "))
+
+print("Birthday")
+birth_month = int(input("Month: "))
+birth_day = int(input("Day: "))
+birth_year = int(input("Year: "))
+
+
+if curr_month > 12 or birth_month > 12:
+    print("Invalid input")
+    int(input("Month: "))
+
+
+
+
+year_diff = curr_year - birth_year
+print(year_diff)
+
+
+
+
+
+
+answer = print("You are " + " " + " years old.")
