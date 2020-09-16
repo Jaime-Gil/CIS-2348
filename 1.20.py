f2b2a9e8f293bc5242d75e8a57c6e8f15261bb7f
Jaime Gil <jgil3@cougarnet.uh.edu>
2020-09-16 11:37:27 -0500

    1.20 Zylabs

---
+++ 1.20.py
@@ -0,0 +1,18 @@
+'''
+Jaime Gil
+1788946
+'''
+
+print("Enter integer:")
+userNum = int(input())
+print("You entered:", userNum)
+userNumSquared = userNum * userNum
+userNumCubed = userNum * userNum * userNum
+print(userNum, "squared is", userNumSquared)
+print("And", userNum, "cubed is", userNumCubed, "!!")
+print("Enter another integer:")
+user_num2 = int(input())
+addAns = userNum + user_num2
+proAns = userNum * user_num2
+print(userNum, "+", user_num2, "is", addAns)
+print(userNum, "*", user_num2, "is", proAns)
