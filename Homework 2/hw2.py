cb2298a595e5972d83f18ae70622fee66c4d8e05
Jaime-Gil <70463107+Jaime-Gil@users.noreply.github.com>
2020-10-07 10:52:45 -0500

    HW2 setup

---
+++ Homework 2/hw2.py
@@ -0,0 +1,10 @@
+"""
+Jaime Gil
+1788946
+"""
+datefile = open("C:\\Users\\jaime\\OneDrive\\Documents\\Jaimes College Classes\\2348 HOMEWORK\\inputDates.txt", "r")
+def dateformat(month,day,year):
+    for date in datefile:
+        print(date)
+
+print(dateformat)
