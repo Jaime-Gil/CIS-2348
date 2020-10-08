040a0d70a3ad5e34df34f2941f689b8069a400f8
Jaime-Gil <70463107+Jaime-Gil@users.noreply.github.com>
2020-10-08 11:31:15 -0500

    Update hw2.py

--- Homework 2/hw2.py
+++ Homework 2/hw2.py
@@ -2,9 +2,18 @@
 Jaime Gil
 1788946
 """
-datefile = open("C:\\Users\\jaime\\OneDrive\\Documents\\Jaimes College Classes\\2348 HOMEWORK\\inputDates.txt", "r")
-def dateformat(month,day,year):
-    for date in datefile:
-        print(date)
+datefile = input()
+opened_f = open(datefile,'r')
+month_dict = {'January':1,'February':2,'March':3,'April':4,'May':5,'June':6,'July':7,'August':8,'September':9,'October':10,'November':11,'December':12}
+valid_dates = []
+file_dates = []
 
-print(dateformat)
+for line in opened_f:
+    file_dates.append(line.strip())
+    for item in range(len(file_dates)):
+        if (file_dates[item].find("/") == -1):
+            valid_dates.append(file_dates[item])
+
+print(file_dates)
+print(valid_dates)
+    
